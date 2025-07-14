76bc069237846f7b82623973583630011388f41a
Farhan Siddiqui <farhaansiddiqui7861@gmail.com>
2025-07-15 00:24:11 +0530

    Update app name in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
         </div>
       </div>
       <div className="footer-bottom">
-        &copy; {new Date().getFullYear()} InstaPDF. All rights reserved.
+        &copy; {new Date().getFullYear()} LinkSnap. All rights reserved.
       </div>
     </footer>
   )
